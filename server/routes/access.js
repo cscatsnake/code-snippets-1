e9e673ddd00e92d8d4a8165be89f05e69cd51db5
Julian Babon <babonjmc@gmail.com>
2023-05-18 15:55:53 -0400

    finally got front/server/db talking

--- server/routes/access.js
+++ server/routes/access.js
@@ -5,14 +5,22 @@ const sessionController = require('../controllers/sessionController');
 const router = express.Router();
 
 router.post('/login',accessController.verifyUser, (req,res) => {
-  return res.redirect('/');
+  return res.redirect('/snippets');
 });
 	
 
 
-router.post('/signup',accessController.createUser, (req,res) => {
-  return res.redirect('/');
-});
+router.post('/signup', 
+  (req, res, next) => {
+    console.log('Received signup request');
+    console.log('Request body:', req.body);
+    next();
+  },
+  accessController.createUser, 
+  (req,res) => {
+    const {first, last} = req.body;
+    return res.status(201).json({first,last});
+  });
 
 
 module.exports = router;
